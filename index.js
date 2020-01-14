255b07e4bdf77e3a479beb22d170b4a6dc42fe76
AmyBraswell <braswellamy@mac.com>
2020-01-14 13:38:56 -0600

    tried to use Cors-anywhere

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 'use strict';
 
 
-const insultUrl="https://insult.mattbas.org/api/insult.html";
-
-var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-
-
+const insultUrl="https://insult.mattbas.org/api/insult";
 
 
 
 
 //////////////// Watch User Form //////////////////////
 
+
+function watchUserForm() {
+    $('form').submit(event => {
+        event.preventDefault(); 
+
+ 
+// Tried to use Cors-anywhere code snippet but I don't think I am passing the correct information
+// Source: https://github.com/Rob--W/cors-anywhere
+
+    /*   
+    jQuery.ajaxPrefilter(function(options) {
+            if (options.crossDomain && jQuery.support.cors) {
+                options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            }
+        });
+    */
+
+        fetch(insultUrl)
+        .then(response => {
+        
+          if (response.ok) {
+          return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        
+        .then((myJson => {
+          //renderResult(myJson);
+          console.log(myJson);
+        }))
+        .catch(err => {
+          $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            }
+          ); 
+        }) 
+
+
+      };
+
+
+
+    $(watchUserForm);
 
 
 
--- scripts/loadCallback.js
+++ scripts/loadCallback.js
@@ -1,34 +1,3 @@
-function watchUserForm() {
-    $('form').submit(event => {
-        event.preventDefault();        
- 
-        fetch(insultUrl)
-        .then(response => {
-            console.log(result);
-        
-            /*
-          if (response.ok) {
-          return response.json();
-          }
-          throw new Error(response.statusText);
-        })
-
-
-        .then((myJson => {
-          //renderResult(myJson);
-          console.log(myJson);
-        }))
-
-
-                .catch(err => {
-          $('#js-error-message').text(`Something went wrong: ${err.message}`);
-
-
-                }); 
-               */
-
-        }) 
-
-
-      });
-    }
+let jsondata = function(testdata){
+    console.log(testdata);
+}
